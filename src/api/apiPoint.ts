61c01c446ac035c08490b6a6ff2e01891573d4e8
Егор Дыбов <egordybov@MacBook-Air-Egor.local>
2025-09-04 14:39:55 +0300

    refacrtor: changed from http to https

--- src/api/apiPoint.ts
+++ src/api/apiPoint.ts
@@ -1,7 +1,7 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const getWeekWeather = (city: string, days: number = 7) => {
-    return `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=${days}&aqi=yes&alerts=no`
+    return `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=${days}&aqi=yes&alerts=no`
 }
 
 
