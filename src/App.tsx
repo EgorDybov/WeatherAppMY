5a807a4fb85a998bba9ac7ac08cc7577913272ea
Егор Дыбов <egordybov@MacBook-Air-Egor.local>
2025-09-05 15:22:51 +0300

    refactor: changed imports, added adaptive design

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import AppLayout from "./components/AppLayout";
 import Modal from "./components/modal/Modal";
 import MainPage from "./components/MainPage";
 
-import './theme.scss'
+import './styles/main.scss'
 
 function App() {
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
